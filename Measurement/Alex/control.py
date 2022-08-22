f1549633bc7bb5f9631c764ce875253a78ba7837
Alex Liebman-Pelaez <alexliebmanp@gmail.com>
2022-08-22 10:33:14 -0700

    added ability to easily add metadata to saved files via a dictionary.

--- Measurement/Alex/control.py
+++ Measurement/Alex/control.py
@@ -7,7 +7,7 @@ import time
 import numpy as np
 import matplotlib.pyplot as plt
 
-def save_to_file(fname, data, header, metadata=None):
+def save_data(fname, data, header, metadata=None):
     '''
     utility function for saving data to a file, with optional metadata
 
@@ -15,13 +15,18 @@ def save_to_file(fname, data, header, metadata=None):
         - fname(string):           full path to datafile
         - data(array):             (n,m) array containing data
         - header(array):           (m) array of strings labeling each column
-        - metadata(str):           in progress
+        - metadata(dict):          a dictionary of parameters to store at the top of the datafile under [Metadata]
 
     returns: None
     '''
     if not(len(header) == len(data[0,:])):
         raise ValueError('number of header items does not match number of data columns.')
     with open(fname, 'w') as f:
+        if not(metadata==None):
+            f.write('[Metadata]\n')
+            for key, value in metadata.items():
+                f.write(f'{key}:\t{value}\n')
+            f.write('[Data]\n')
         for item in header:
             f.write(str(item)+'\t')
         f.write('\n')
@@ -30,20 +35,21 @@ def save_to_file(fname, data, header, metadata=None):
                 f.write(str(item)+'\t')
             f.write('\n')
 
-
-filename = 'G:/Shared drives/Orenstein Lab/Data/alex/20220818_strain_cell_capacitor_noise_25ft_coax_300kHz.txt'
-num_points = 1000
-t = np.zeros(num_points)
-cap = np.zeros(num_points)
-t0 = time.time()
-i = 0
-t_old = t0
-while i < num_points:
-    t_new = time.time()
-    if t_new - t_old > 0.1:
-        c = sc.get_cap()
-        cap[i] = c
-        t[i] = t_new - t0
-        i = i + 1
-        t_old = t_new
-save_to_file(filename, np.transpose([t, cap]), ['Time', 'Cap'])
+def measure_strain_cell_capacitor(fname, sc, num_points=1000, dt_min=0.1):
+    '''
+    mesaures the
+    '''
+    t = np.zeros(num_points)
+    cap = np.zeros(num_points)
+    t0 = time.time()
+    i = 0
+    t_old = t0
+    while i < num_points:
+        t_new = time.time()
+        if t_new - t_old > dt_min:
+            c = sc.get_cap()
+            cap[i] = c
+            t[i] = t_new - t0
+            i = i + 1
+            t_old = t_new
+    save_data(f, np.transpose([t, cap]), ['Time', 'Cap'])

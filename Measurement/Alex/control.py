7dcaf0b0bb6a9734bcee508d3710f87420d9de24
Alex Liebman-Pelaez <alexliebmanp@gmail.com>
2022-08-30 16:05:53 -0700

    added a few functions that I wrote on lab computer

--- Measurement/Alex/control.py
+++ Measurement/Alex/control.py
@@ -1,3 +1,7 @@
+'''
+Main file for controlling lab equipment and orchestrating measurements
+'''
+
 get_ipython().run_line_magic('matplotlib', 'notebook')
 import zhinst.utils as ziutils
 import instruments.newport as newport
@@ -14,6 +18,7 @@ import threading
 import ipywidgets as widgets
 from pyanc350.v2 import Positioner
 import OrensteinLab.Instrument.Lakeshore.Lakeshore335 as ls
+from strain_control.strain_client import StrainClient
 
 # confiure system based on config file
 with open(os.path.dirname(__file__)+ r'\..\..\Configuration.txt', "r") as f_conf:
@@ -230,3 +235,45 @@ def get_angle_range(startangle, endangle, stepsize=0.):
         return np.linspace(startangle, newendangle, int(roundsteps)+1)
     else:
         return np.linspace(startangle, endangle, int(roundsteps)+1)
+
+def save_to_file(fname, data, header, metadata=None):
+    '''
+    utility function for saving data to a file, with optional metadata
+
+    args:
+        - fname(string):           full path to datafile
+        - data(array):             (n,m) array containing data
+        - header(array):           (m) array of strings labeling each column
+        - metadata(str):           in progress
+
+    returns: None
+    '''
+    if not(len(header) == len(data[0,:])):
+        raise ValueError('number of header items does not match number of data columns.')
+    with open(fname, 'w') as f:
+        for item in header:
+            f.write(str(item)+'\t')
+        f.write('\n')
+        for line in data:
+            for item in line:
+                f.write(str(item)+'\t')
+            f.write('\n')
+
+''' a script I wrote for collecting data on capacitive sensor
+filename = 'G:/Shared drives/Orenstein Lab/Data/alex/20220818_strain_cell_capacitor_noise_25ft_coax_300kHz.txt'
+num_points = 1000
+t = np.zeros(num_points)
+cap = np.zeros(num_points)
+t0 = time.time()
+i = 0
+t_old = t0
+while i < num_points:
+    t_new = time.time()
+    if t_new - t_old > 0.1:
+        c = sc.get_cap()
+        cap[i] = c
+        t[i] = t_new - t0
+        i = i + 1
+        t_old = t_new
+save_to_file(filename, np.transpose([t, cap]), ['Time', 'Cap'])
+'''
